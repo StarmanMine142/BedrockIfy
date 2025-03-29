41abe7432eb091509bc2f743a8166d83bc4d15f6
Juan Carlos <juancarloscp52@gmail.com>
2025-03-29 12:41:51 +0100

    Small cleanup

--- src/main/java/me/juancarloscp52/bedrockify/client/features/worldColorNoise/WorldColorNoiseSampler.java
+++ src/main/java/me/juancarloscp52/bedrockify/client/features/worldColorNoise/WorldColorNoiseSampler.java
@@ -1,7 +1,6 @@
 package me.juancarloscp52.bedrockify.client.features.worldColorNoise;
 
 import com.google.common.collect.ImmutableList;
-import me.juancarloscp52.bedrockify.client.BedrockifyClient;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.noise.OctaveSimplexNoiseSampler;
@@ -19,12 +18,12 @@ public class WorldColorNoiseSampler {
     }
 
     public int applyNoise(BlockPos pos, int previousColor, float scale, float intensity){
-        double noiseValue = BedrockifyClient.getInstance().worldColorNoiseSampler.getSample(pos.getX(),pos.getZ(),scale);
+        double noiseValue = this.getSample(pos.getX(),pos.getZ(),scale);
         double d1=noiseValue*2; // round noiseValue to nearest half number to reduce softness
         d1=Math.round(d1);
         noiseValue=d1/2;
 
-        d1=BedrockifyClient.getInstance().worldColorNoiseSampler.getSample(pos.getX(),pos.getZ(),0.2f); // get noise value for per-block noise
+        d1=this.getSample(pos.getX(),pos.getZ(),0.2f); // get noise value for per-block noise
 
         int noiseValueInt=blend(previousColor,(float)noiseValue*intensity); // combine noiseValue and per-block noise value
         return blend(noiseValueInt,(float)d1*(intensity*0.6f));
